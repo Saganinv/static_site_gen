fab27294db0a8974499533f90b8a57859c55d14c
Saganinv <saganinvestments@gmail.com>
2025-03-26 10:12:19 +1300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    text_node = TextNode("Pick up, Pizza Hutt", "NORMAL_TEXT", "www.google.com")
+    print(TextNode)
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "NORMAL_TEXT"
+    BOLD_TEXT = "BOLD_TEXT"
+    ITALIC_TEXT = "ITALIC_TEXT"
+    CODE_TEXT = "CODE_TEXT"
+    LINK_TEXT = "LINK_TEXT"
+    IMAGE_TEXT = "IMAGE_TEXT"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    
+    def __repr__(self):
+        return f"Text: {self.text}, Type: {self.text_type}, URL: {self.url}"
+    
